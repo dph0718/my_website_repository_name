df8cb1c488c09e595ae0bcd026542f7efb3eafbe
David <dph0718@gmail.com>
2025-02-21 14:15:45 -0700

    Connects to database

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
+import pool from "./db";
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello from the backend!");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Database connected: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error connecting to database");
+  }
+  // res.send("Hello from the backend!");
 });
 
 app.listen(PORT, () => {
