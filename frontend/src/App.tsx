3610a707fe297605f67a6e03ee14c7f6715c5f4e
David <dph0718@gmail.com>
2025-02-21 16:44:25 -0700

    Connect frontend to db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import api from "./api/api";
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    api.get("/")
+      .then(res => setMessage(res.data))
+      .catch(err => {
+        console.error("Error fetching data: ", err);
+        setMessage("Failed to load data,");
+      })
+  }, [])
 
   return (
     <>
+      <div>
+        <p>{message}</p>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
