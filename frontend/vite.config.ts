535ad658665d0fdb351b6d4c02d9902eee5cf831
David <dph0718@gmail.com>
2025-02-27 23:36:03 -0700

    Add data for vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://my-website-repository-web-service.onrender.com"
+    }
+  },
   plugins: [react()],
 })
