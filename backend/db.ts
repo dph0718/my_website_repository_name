3785291a3e3e53b63c95be215f51bd45137a64dd
David <dph0718@gmail.com>
2025-02-27 23:16:31 -0700

    Update connection for local dev & deployed

--- backend/db.ts
+++ backend/db.ts
@@ -3,12 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
+const isDev = process.env.NODE_ENV === "dev";
+
+const pool = isDev ? new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || "5432"),
+}) : new Pool({
+  connectionString: process.env.DATABASE_URL
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: parseInt(process.env.DB_PORT || "5432"),
 });
 
 export default pool;
